fdcddec8282614f5e3ea01917995f1c8e0f3936b
toddtlee48 <toddtlee48@gmail.com>
2019-05-19 16:23:45 -0400

    Final copy
    Final copy of 2nd TechDegree project for pagination.

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
+
 //Global Variables
 const studentsList = document.querySelectorAll(".student-item");
 const numOfPages = Math.ceil(studentsList.length / 10);
 
 // Function that loops through and shows the appropriate students per page. 
-
 const showPage = (studentsList, page) => {
   start_index = (page * 10) - 10;
   end_index = page * 10;
@@ -23,37 +23,37 @@ const showPage = (studentsList, page) => {
 showPage(studentsList, 1);
 
 const appendPages = list => {
-  // create div element on page and set class
+  // Creates div element on page and sets class.
   const div = document.createElement("div");
-  // assign page class to a variable
+  // Assigns page class to a variable.
   const page = document.querySelector(".page");
   page.appendChild(div);
   div.className = "pagination";
-  // add ul to div
+  // Adds ul to div.
   const ul = document.createElement("ul");
   div.appendChild(ul);
-  // for loop to add li, and a tags to every page
+  // For loop to add li and a tags to every page. 
   for (let i = 1; i <= numOfPages; i += 1) {
-    // add li tags to the div
+    // Adds li tags to the div.
     const li = document.createElement("li");
     ul.appendChild(li);
-    // add a tags to the li
+    // Adds a tags to the li.
     const a = document.createElement("a");
     li.appendChild(a);
     a.setAttribute("href", "#");
     a.textContent = i;
-    //click handler for a tags 
+    //Clickhandler for a tags.
     a.addEventListener("click", e => {
       const aLinks = document.querySelectorAll("a");
       let page = e.target;
       page.className = "active";
-      // loop through all a tags and highlights current page
+      // For loop that goes through all a tags and highlights the current page.
       for (let i = 0; i < aLinks.length; i += 1) {
         if (aLinks[i] !== page) {
           aLinks[i].className = "";
         }
       }
-      // Calls the show page function
+      // Calls the show page function.
       showPage(studentsList, page.textContent);
     });
   }
